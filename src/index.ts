6e58fcffa058aaa0da5b75a1b5b34a323cc7c39d
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-09-21 01:59:22 -0500

    feature: add current state to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,69 @@
-// TODO: final result goes here
+import { Config, Effect, Layer, Schema } from "effect";
+
+import { FetchError, JsonError } from "./errors";
+import { Pokemon } from "./schemas";
+
+class PokemonCollection extends Effect.Service<PokemonCollection>()(
+	"PokemonCollection",
+	{
+		succeed: ["staryu", "perrserker", "flaaffy"],
+	},
+) {}
+
+class PokeApiUrl extends Effect.Service<PokeApiUrl>()("PokeApiUrl", {
+	effect: Effect.gen(function* () {
+		const baseUrl = yield* Config.string("BASE_URL");
+
+		return ({ name }: { name: string }) => `${baseUrl}/pokemon/${name}`;
+	}),
+}) {}
+
+class PokeApi extends Effect.Service<PokeApi>()("PokeApi", {
+	effect: Effect.gen(function* () {
+		const pokemonCollection = yield* PokemonCollection;
+		const buildPokeApiUrl = yield* PokeApiUrl;
+
+		return {
+			getPokemon: Effect.gen(function* () {
+				const requestUrl = buildPokeApiUrl({ name: pokemonCollection[0] });
+
+				const response = yield* Effect.tryPromise({
+					try: () => fetch(requestUrl),
+					catch: () => new FetchError(),
+				});
+
+				if (!response.ok) {
+					return yield* new FetchError();
+				}
+
+				const json = yield* Effect.tryPromise({
+					try: () => response.json(),
+					catch: () => new JsonError(),
+				});
+
+				return yield* Schema.decodeUnknown(Pokemon)(json);
+			}),
+		};
+	}),
+	dependencies: [PokemonCollection.Default, PokeApiUrl.Default],
+}) {}
+
+const program = Effect.gen(function* () {
+	const pokeApi = yield* PokeApi;
+
+	return yield* pokeApi.getPokemon;
+});
+
+const MainLayer = Layer.mergeAll(PokeApi.Default);
+
+const liveProgram = program.pipe(Effect.provide(MainLayer));
+
+const main = liveProgram.pipe(
+	Effect.catchTags({
+		FetchError: () => Effect.succeed("Fetch error"),
+		JsonError: () => Effect.succeed("Json error"),
+		ParseError: () => Effect.succeed("Parse error"),
+	}),
+);
+
+Effect.runPromise(main).then(console.log);
